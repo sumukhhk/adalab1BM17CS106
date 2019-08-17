1545b7e2aad3e0bf50e9a54481e5b12c90e61529
sumukhhk <51911251+sumukhhk@users.noreply.github.com>
2019-08-17 10:40:56 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int binaryroot()
+{
+	int low=0,high,mid,n,ans;
+	cout<<"Enter the number";
+	cin>>n;
+	high=n-1;
+	while(low<=high)
+	{
+	 mid=(low+high)/2;
+	 if((mid*mid)==n)
+		return mid;
+	else if((mid*mid)<n)
+		{low=mid+1;
+		 ans=mid;
+		}
+	else
+  		high=mid-1;
+	}
+	return ans;
+
+}
+int main()
+{
+	clock_t start,end;
+	start=clock();
+	int x=binaryroot();
+	cout<<"The square root is "<<x<<endl;
+	end=clock();
+	double t=double(end-start)/double(CLOCKS_PER_SEC);
+	cout<<t;
+	return 0;
+}
