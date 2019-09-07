22a63cc587448e2b164f5ecfc72b731ac6738b58
sumukhhk <51911251+sumukhhk@users.noreply.github.com>
2019-09-07 09:14:54 +0530

    Update selectiontest.cpp

--- selectiontest.cpp
+++ selectiontest.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	int n,temp,k,min,count=0,count1=0;
+	int n,temp,k,min,count=0;
 	cout<<"Enter the array length:";
 	cin>>n;
 	int a[n];
@@ -16,16 +16,17 @@ int main()
 	for(int i=0;i<n-1;i++)
 		{min=i;
 		for(int j=i+1;j<n;j++)
-			{if(a[j]<a[min])
+			{count++;
+			if(a[j]<a[min])
 				min=j;
-				count1++;
+				
 			}
 		temp=a[min];
 		a[min]=a[i];
 		a[i]=temp;
-		count++;
+		
 		}
-		cout<<"Count:"<<count*count1<<endl;
+		cout<<"Count:"<<count<<endl;
 	end=clock();
 	double t=double(end-start)/double(CLOCKS_PER_SEC);
 	cout<<t<<" is the time taken";
