e67577fc62aa2b21da81ba16739b426bf54bf217
sumukhhk <51911251+sumukhhk@users.noreply.github.com>
2019-11-09 13:23:51 +0530

    Update 9a.cpp

--- 9a.cpp
+++ 9a.cpp
@@ -1,44 +1,38 @@
-
-#include<bits/stdc++.h> 
-using namespace std; 
-  
-
+#include<iostream>
+using namespace std;
 int minCoins(int coins[], int m, int V) 
+{  
+if (V == 0) return 0;  
+int res = INT_MAX; 
+for (int i=0; i<m; i++) 
 { 
-   int res = INT_MAX; 
-   if (V == 0) 
-      return 0; 
-
-  
-   for (int i=0; i<m; i++) 
-   { 
-     if (coins[i] <= V) 
-     { 
-         int sub_res = minCoins(coins, m, V-coins[i]); 
-         if (sub_res != INT_MAX && sub_res + 1 < res) 
-            res = sub_res + 1; 
-     } 
-   } 
-   return res; 
+	if (coins[i] <= V) 
+	{ 
+		int sub_res = minCoins(coins, m, V-coins[i]); 
+		if (sub_res != INT_MAX && sub_res + 1 < res) 
+			res = sub_res + 1; 
+	} 
 } 
-
-
-
-  
-
+return res; 
+}  
 int main() 
 { 
-    int coins[10], m, i, V; 
-    cout<<"Enter the coins available : ";
-    cin>>m;
-    cout<<"\nEnter the coins values : ";
-    for(i=0; i<m; i++)
-    cin>>coins[i];
-    cout<<"\nEnter the total value : ";
-    cin>>V; 
-    cout << "\nMinimum coins required is " << minCoins(coins, m, V) <<endl; 
-    cout<<coin[sub_res[v]];
-    
-    return 0; 
+	int coins[20],m,V;  
+  cout<<"Enter no. of coins : ";
+  cin>>m;
+  cout<<"Enter coins values:";  
+  for( int i =0;i<m;i++)
+      cin>>coins[i];
+  cout<<"Enter value : ";
+  cin>>V;
+	cout << "Minimum coins required is : "<< minCoins(coins, m, V); 
+	return 0; 
 } 
 
+/*
+OUTPUT:
+Enter no. of coins : 3                                                                                                                               
+Enter coins values:10 20 100                                                                                                                         
+Enter value : 270                                                                                                                                    
+Minimum coins required is : 6
+*/
